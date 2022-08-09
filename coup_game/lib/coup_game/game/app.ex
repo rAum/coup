1c8dc3af658a85be66ca7abad7dfc736d8a3879e
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-08-09 17:21:21 +0200

    Fix dialyzer error no_return

--- coup_game/lib/coup_game/game/app.ex
+++ coup_game/lib/coup_game/game/app.ex
@@ -1,22 +1,22 @@
 defmodule CoupGame.Game.App do
   use Supervisor
-  alias CoupGame.Game.{Rooms, RoomsCleaner, PlayerNames}
+  alias CoupGame.Game.{Rooms, PlayerNames}
 
   @registry :game_rooms
 
-  def start_link(init_arg) do
-    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  @impl true
-  def init([]) do
+  @impl Supervisor
+  def init(_) do
     children = [
       {Registry, [keys: :unique, name: @registry]},
       PlayerNames,
       Rooms,
-      #RoomsCleaner,
     ]
-    Supervisor.init(children, [strategy: :one_for_one, name: __MODULE__])
+    opts = [strategy: :one_for_one]
+    Supervisor.init(children, opts)
   end
 
 end
